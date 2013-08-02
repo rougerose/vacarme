105857b69da0696c56317d968b03f3d4a7cd7987
christophe le drean <christophe@rouge-rose.net>
2013-08-02 08:00:51 +0200

    version 3 : - suppression fichiers version précédente - inuit

--- squelettes/lang/local_fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS[$GLOBALS['idx_lang']] = array(
-        'traduction' => 'ce texte est &eacute;galement disponible en version fran&ccedil;aise',
-        'publie' => 'publi&eacute; dans',
-        'entretien' => 'entretien r&eacute;alis&eacute;',
-        'propos' => 'propos recueillis'
-);
-?>
