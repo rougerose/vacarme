6e469d02e513a4fd9916db23b2cb185a0decd3a1
christophe le drean <christophe@rouge-rose.net>
2013-08-04 22:31:55 +0200

    navigation icones

---
+++ lang/local_fr.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+   // C
+   'close'      => "Fermer",
+   
+   // N
+   'navigation' => "Navigation"
+   
+   );
+?>
