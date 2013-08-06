1be975c31de324feb025b2112786f77b1ea04bac
christophe le drean <christophe@rouge-rose.net>
2013-08-06 13:45:18 +0200

    navigation : version desk

--- js/vacarme.js
+++ js/vacarme.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
-    //var a = $( document.activeElement ); console.log(a);
-    
-    $("#formulaire_recherche form:not:(.active-search)").click(function(){
-        $(this).addClass("active-search");
-    });
+    // $("#formulaire_recherche form:not:(.active-search)").click(function(){
+    //     $(this).addClass("active-search");
+    // });
 
-    $("#formulaire_recherche form input.search__field").blur(function(){
-        $(this).parent().parent().removeClass("active-search");
-    });
-    
-    // formulaire recherche
-     //$("#formulaire_recherche").each(function(){
-
-    //     var $sf = $(this).find(".search__field"),
-    //     $f = $(this).find("form");
-    //     $()
-    //});
+    // $("#formulaire_recherche form input.search__field").blur(function(){
+    //     $(this).parent().parent().removeClass("active-search");
+    // });
 });
