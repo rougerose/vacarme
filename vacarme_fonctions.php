fe2917ebb6f1e4b11171a29e54823cd9cb943f8b
christophe le drean <christophe@rouge-rose.net>
2013-08-08 19:35:39 +0200

    article (début)

---
+++ vacarme_fonctions.php
@@ -0,0 +1,42 @@
+<?php 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+function prenom_nom($texte) {
+    if (strstr($texte, "*")) {
+        $prenom = prenom($texte);
+        $nom = nom($texte);
+        if ($prenom && $nom) {
+            return $prenom.' '.$nom;
+        } else {
+            $prenom.$nom;
+        }
+    } else {
+        return $texte;
+    }
+}
+
+function prenom($texte) {
+    if(strstr($texte,"*")) {
+        if ($prenom = trim(preg_replace('/^(.*)\*(.*)/', '$2', $texte))) {
+            return $prenom;
+        } else {
+            return "";
+        }
+    } else {
+        return "";
+    }
+}
+
+function nom($texte) {
+    if(strstr($texte,"*")) {
+        if ($nom = trim(preg_replace('/^(.*)\*(.*)/', '$1', $texte))) {
+            return $nom;
+        } else {
+            return "";
+        }
+
+    } else {
+        return "";
+    }
+}
+?>
---
+++ vacarme_options.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+// en dev, pas de cache
+define('_NO_CACHE', 1);
+
+// intertitres
+$GLOBALS['debut_intertitre'] = "\n<h2 class='spip'>";
+$GLOBALS['fin_intertitre'] = "</h2>\n";
+
+?>
