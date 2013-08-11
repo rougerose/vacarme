93cea5397fcd2040e183a163e6645b95d64e6668
christophe le drean <christophe@rouge-rose.net>
2013-08-11 19:58:09 +0200

    article suite

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -1,11 +1,26 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-   // C
-   'close'      => "Fermer",
-   
-   // N
-   'navigation' => "Navigation"
+    // A
+    'author_about_plural'   => "À propos des auteurs",
+    'author_about_singular' => "À propos de l’auteur",
+    'author_plural'         => "Les auteurs",
+    'author_singular'       => "L’auteur",
+    
+    // C
+    'close'                 => "Fermer",
+    
+    // I
+    'info_ps'               => 'Post-scriptum',
+    
+    // N
+    'navigation'            => "Navigation",
+    
+    // P
+    'publication_about' => "À propos de l’article",
+    
+    // R
+    'read_also'             => 'Lire également'
    
    );
 ?>
