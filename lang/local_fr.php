2ab6b1e0f40be8e29b2ba3e10bd6d8a591e7dfe9
christophe le drean <christophe@rouge-rose.net>
2013-09-21 14:55:21 +0200

    recherche et quelques autres corrections

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,8 +2,12 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
     // A
+    'article_number' => '1 article',
+    'articles_number' => '@nb@ articles',
     'author_about_plural'   => "À propos des auteurs",
     'author_about_singular' => "À propos de l’auteur",
+    'author_number' => '1 auteur',
+    'authors_number' => '@nb@ auteurs',
     'author_plural'         => "Les auteurs",
     'author_singular'       => "L’auteur",
     
@@ -22,11 +26,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'info_contact_facebook' => "Via Facebook",
     'info_contact_mail' => "Par email",
     'info_contact_newsletter' => "S'inscrire à la newsletter",
+    'info_page' => 'Page',
     'info_ps'               => 'Post-scriptum',
     'issue_about' => "À propos de ce numéro",
 
+    // K
+    'keyword_number' => '1 mot-clé',
+    'keywords_number' => '@nb@ mot-clés',
+
     // M
-    'more' => 'Lire <em> → </em>',
+    'more' => '<em>&#8594;</em>',
     
     // N
     'navigation'            => "Navigation",
@@ -36,14 +45,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'menus-description-navigation-item-recherche' => 'Lien pour afficher le formulaire de recherche',
     
     // P
-    'page'                  => 'page&nbsp;@debut@.',
     'page_abrev'            => 'pp.&nbsp;@debut@-@fin@.',
+    'page_next' => 'Page suivante',
+    'page_number'                  => 'page&nbsp;@debut@.',
+    'page_previous' => 'Page précédente',
     'publication_about'     => "À propos de l’article",
     'published_in'          => 'Publiée dans',
     'published_date'        => 'Publiée le',
     
     // R
     'read_also'             => 'Lire également',
+
+    // S
+    'section_number' => '1 rubrique',
+    'sections_number' => '@nb@ rubriques',
     
     // T
     'table_of_content' => "Sommaire",
