9a34d454945df951640197cf32f1b765a4789797
christophe le drean <christophe@rouge-rose.net>
2013-10-16 08:58:30 +0200

    corrections navigation et page d'accueil

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -2,89 +2,90 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
     // A
-    'abonner' => "S’abonner",
-    'acheter' => 'Acheter',
-    'apercu_info_article' => "L'intégralité de cet article est disponible dans <em>@titre_numero@</em> actuellement en vente dans votre librairie ou <a href='http://commande.vacarme.org'>en ligne</a>.",
-    'apercu_info_numero' => "Vacarme aime la gratuité, mais ce numéro est actuellement en vente en librairie. Seuls les articles précédés de cette icône <span class='icon icon--unlock'></span> sont disponibles dans leur intégralité.",
-    'article_auteur' => "1 article publié dans Vacarme",
-    'articles_auteur' => '@nb@ articles publiés dans Vacarme',
-    'article_auteur_aucun' => 'Aucun article publié dans <i>Vacarme</i> à ce jour.',
-    'article_number' => '1 article',
-    'articles_number' => '@nb@ articles',
-    'auteurs_tous_back' => '⟵ Tous les auteurs',
-    'author_about_plural'   => "À propos des auteurs",
-    'author_about_singular' => "À propos de l’auteur",
-    'author_number' => '1 auteur',
-    'authors_number' => '@nb@ auteurs',
-    'authors'         => "Les auteurs",
-    'author'       => "L’auteur",
+    'abonner'                                     => "S’abonner",
+    'acheter'                                     => 'Acheter',
+    'apercu_info_article'                         => "L'intégralité de cet article est disponible dans <em>@titre_numero@</em> actuellement en vente dans votre librairie ou <a href='http://commande.vacarme.org'>en ligne</a>.",
+    'apercu_info_numero'                          => "Vacarme aime la gratuité, mais ce numéro est actuellement en vente en librairie. Seuls les articles précédés de cette icône <span class='icon icon--unlock'></span> sont disponibles dans leur intégralité.",
+    'article_auteur'                              => "1 article publié dans Vacarme",
+    'articles_auteur'                             => '@nb@ articles publiés dans Vacarme',
+    'article_auteur_aucun'                        => 'Aucun article publié dans <i>Vacarme</i> à ce jour.',
+    'article_number'                              => '1 article',
+    'articles_number'                             => '@nb@ articles',
+    'auteurs_tous_back'                           => '⟵ Tous les auteurs',
+    'author_about_plural'                         => "À propos des auteurs",
+    'author_about_singular'                       => "À propos de l’auteur",
+    'author_number'                               => '1 auteur',
+    'authors_number'                              => '@nb@ auteurs',
+    'authors'                                     => "Les auteurs",
+    'author'                                      => "L’auteur",
     
     // C
-    'category_plural'       => 'Catégories',
-    'category_singular'     => 'Catégorie',
-    'close'                 => "Fermer",
-
+    'category_plural'                             => 'Catégories',
+    'category_singular'                           => 'Catégorie',
+    'close'                                       => "Fermer",
+    
     // D
-    'documents_fichier' => 'Documents associés',
-    'documents_image' => 'Images associées',
-
+    'documents_fichier'                           => 'Documents associés',
+    'documents_image'                             => 'Images associées',
+    
     // E
-    'esprit_de_suite' => "Esprit de suite",
+    'esprit_de_suite'                             => "Esprit de suite",
     
     // I
-    'info_contact' => "Gardons le contact",
-    'info_contact_coordonnees' => "Par courrier",
-    'info_contact_facebook' => "Via Facebook",
-    'info_contact_mail' => "Par email",
-    'info_contact_newsletter' => "S’inscrire à la newsletter",
-    'info_page' => 'Page',
-    'info_ps'               => 'Post-scriptum',
-    'issue_about' => "À propos de ce numéro",
-
+    'info_contact'                                => "Gardons le contact",
+    'info_contact_coordonnees'                    => "Par courrier",
+    'info_contact_facebook'                       => "Via Facebook",
+    'info_contact_mail'                           => "Par email",
+    'info_contact_newsletter'                     => "S’inscrire à la newsletter",
+    'info_page'                                   => 'Page',
+    'info_ps'                                     => 'Post-scriptum',
+    'issue_about'                                 => "À propos de ce numéro",
+    
     // K
-    'keyword_number' => '1 mot-clé',
-    'keywords_number' => '@nb@ mot-clés',
-
+    'keyword_number'                              => '1 mot-clé',
+    'keywords_all'                                 => 'Tous les mots-clés',
+    'keywords_number'                             => '@nb@ mot-clés',
+    
     // M
-    'message_404_title'              => "Page indisponible.",
-    'message_404_desc'               => "Nous sommes désolé-e-s, la page que vous recherchez n’est pas disponible.<br />Revenir à <a href='@urlsite@' class='spip_in'>la page d'accueil du site</a>.",
-    'more_issue' => 'Le sommaire complet',
-    'more_arrow' => '<em>&#8594;</em>',
-    'more_read' => 'Lire <em>&#8594;</em>',
+    'message_404_title'                           => "Page indisponible.",
+    'message_404_desc'                            => "Nous sommes désolé-e-s, la page que vous recherchez n’est pas disponible.<br />Revenir à <a href='@urlsite@' class='spip_in'>la page d'accueil du site</a>.",
+    'more_issue'                                  => 'Le sommaire complet',
+    'more_arrow'                                  => '<em>&#8594;</em>',
+    'more_read'                                   => 'Lire <em>&#8594;</em>',
     
     // N
-    'navigation'            => "Navigation",
-    'navigation_theme' => "Par thème",
-    'navigation_revue' => "La revue",
-    'menus-navigation-item-recherche' => "Lien recherche",
+    'navigation'                                  => "Navigation",
+    'navigation_theme'                            => "Par thème",
+    'navigation_revue'                            => "La revue",
+    'menus-navigation-item-recherche'             => "Lien recherche",
     'menus-description-navigation-item-recherche' => 'Lien pour afficher le formulaire de recherche',
     
     // P
-    'page_abrev'            => 'pp.&nbsp;@debut@-@fin@.',
-    'page_next' => 'Page suivante',
-    'page_number'                  => 'page&nbsp;@debut@.',
-    'pages' => 'pages',
-    'page_previous' => 'Page précédente',
-    'publication_about'     => "À propos de l’article",
-    'published_in'          => 'Publiée dans',
-    'published_date'        => 'Publiée le',
+    'page_abrev'                                  => 'pp.&nbsp;@debut@-@fin@.',
+    'page_next'                                   => 'Page suivante',
+    'page_number'                                 => 'page&nbsp;@debut@.',
+    'pages'                                       => 'pages',
+    'page_previous'                               => 'Page précédente',
+    'publication_about'                           => "À propos de l’article",
+    'published_in'                                => 'Publiée dans',
+    'published_date'                              => 'Publiée le',
     
     // R
-    'read_also'             => 'Lire également',
-    'redaction_en_chef' => "Rédaction en chef",
-
+    'read_also'                                   => 'Lire également',
+    'redaction_en_chef'                           => "Rédaction en chef",
+    
     // S
-    'section_number' => '1 rubrique',
-    'sections_number' => '@nb@ rubriques',
+    'section_number'                              => '1 rubrique',
+    'sections_number'                             => '@nb@ rubriques',
     
     // T
-    'table_of_content' => "Sommaire",
-    'tag_plural'            => 'Mots-clés',
-    'tag_singular'          => 'Mot-clé',
+    'table_of_content'                            => "Sommaire",
+    'tag_plural'                                  => 'Mots-clés',
+    'tag_singular'                                => 'Mot-clé',
     
     // V
-    'version_online'        => "Version en ligne",
-    'version_print'         => "Version imprimée"
+    'version_online'                              => "Version en ligne",
+    'version_print'                               => "Version imprimée"
    
    );
 ?>
