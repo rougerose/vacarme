d905a79f723175608578ad93c98e6b5aa681ab8b
christophe le drean <christophe@rouge-rose.net>
2014-07-10 00:57:01 +0200

    catégorie Tribunes et corrections sur catégorie Entretiens

--- lang/local_fr.php
+++ lang/local_fr.php
@@ -10,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'article_auteur'                              => "1 article publié dans Vacarme",
     'articles_auteur'                             => '@nb@ articles publiés dans Vacarme',
     'article_auteur_aucun'                        => 'Aucun article publié dans <i>Vacarme</i> à ce jour.',
-    'article_integral_admin' => "Vous êtes identifié-e comme administratrice/teur du site.<br>
+    'article_integral_admin'                      => "Vous êtes identifié-e comme administratrice/teur du site.<br>
     Cet article s'affiche pour vous dans <strong>sa version intégrale</strong>.<br>
     Un visiteur sans identification particulière n'aura qu'un aperçu de l'article.",
     'article_number'                              => '1 article',
@@ -22,16 +22,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'authors_number'                              => '@nb@ auteurs',
     'authors'                                     => "Les auteurs",
     'author'                                      => "L’auteur",
-
+    
     // B
-    'back_to_top' => "Revenir en haut",
+    'back_to_top'                                 => "Revenir en haut",
     
     // C
-    'categorie_entretiens_selection' => "Une sélection",
-    'categorie_entretiens_tous' => "Tous les entretiens",
+    'categorie_entretiens_selection'              => "Une sélection",
+    'categorie_entretiens_tous'                   => "Tous les entretiens",
+    'categorie_tribunes_selection'                => "Une sélection",
+    'categorie_tribunes_tous'                     => "Toutes les tribunes",
+    'categories'                                  =>"Catégories",
+    'categorie'                                   =>"Catégorie",
     'category_plural'                             => 'Catégories',
     'category_singular'                           => 'Catégorie',
-    'classe_dans_categorie' => "Classé dans",
+    'classe_dans_categorie'                       => "Classé dans",
     'close'                                       => "Fermer",
     
     // D
@@ -42,9 +46,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'esprit_de_suite'                             => "Esprit de suite",
     
     // I
-    'info_articles' => "Articles",
-    'info_auteurs' => "Auteurs",
-    'info_themes' => "Thèmes et catégories",
+    'info_articles'                               => "Articles",
+    'info_auteurs'                                => "Auteurs",
+    'info_themes'                                 => "Thèmes et catégories",
     'info_contact'                                => "Gardons le contact",
     'info_contact_coordonnees'                    => "Par courrier",
     'info_contact_facebook'                       => "Via Facebook",
@@ -53,12 +57,12 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'info_page'                                   => 'Page',
     'info_ps'                                     => 'Post-scriptum',
     'issue_about'                                 => "À propos de ce numéro",
-    'info_precedent' => "Précédent",
-    'info_suivant' => "Suivant",
-
+    'info_precedent'                              => "Précédent",
+    'info_suivant'                                => "Suivant",
+    
     // J
-    'jour_precedent' => "Journée précédente",
-    'jour_suivant' => "Journée suivante",
+    'jour_precedent'                              => "Journée précédente",
+    'jour_suivant'                                => "Journée suivante",
     
     // K
     'keyword_number'                              => '1 mot-clé',
@@ -68,27 +72,27 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     // M
     'message_404_title'                           => "Page indisponible.",
     'message_404_desc'                            => "Nous sommes désolé-e-s, la page que vous recherchez n’est pas disponible.<br />Revenir à <a href='@urlsite@' class='spip_in'>la page d'accueil du site</a>.",
-    'message_article_postdate' => "Article à paraître le",
-    'message_erreur' => "Une erreur s’est produite",
-    'message_erreur_video_aucune' => "<strong>Une erreur s’est produite</strong><br />aucune vidéo n’est disponible.",
-    'message_erreur_video_absente' => "<strong>Une erreur s’est produite</strong><br />la vidéo demandée n’existe pas.",
+    'message_article_postdate'                    => "Article à paraître le",
+    'message_erreur'                              => "Une erreur s’est produite",
+    'message_erreur_video_aucune'                 => "<strong>Une erreur s’est produite</strong><br />aucune vidéo n’est disponible.",
+    'message_erreur_video_absente'                => "<strong>Une erreur s’est produite</strong><br />la vidéo demandée n’existe pas.",
     'more_issue'                                  => 'Le sommaire complet',
     'more_arrow'                                  => '<em>&#8594;</em>',
     'more_read'                                   => 'Lire <em>&#8594;</em>',
     
     // N
     'navigation'                                  => "Navigation",
-    'navigation_categories'                            => "Catégories",
+    'navigation_categories'                       => "Catégories",
     'navigation_revue'                            => "La revue",
     'menus-navigation-item-recherche'             => "Lien recherche",
     'menus-description-navigation-item-recherche' => 'Lien pour afficher le formulaire de recherche',
-    'nombre_article' => '1 article',
-    'nombre_articles' => '@nb@ articles',
-    'nombre_auteur' => '1 auteur',
-    'nombre_auteurs' => '@nb@ auteurs',
-    'nombre_mot' => "1 thème",
-    'nombre_mots' => "@nb@ thèmes",
-
+    'nombre_article'                              => '1 article',
+    'nombre_articles'                             => '@nb@ articles',
+    'nombre_auteur'                               => '1 auteur',
+    'nombre_auteurs'                              => '@nb@ auteurs',
+    'nombre_mot'                                  => "1 thème",
+    'nombre_mots'                                 => "@nb@ thèmes",
+    
     
     // P
     'page_abrev'                                  => 'pp.&nbsp;@debut@-@fin@.',
@@ -96,42 +100,44 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'page_number'                                 => 'page&nbsp;@debut@.',
     'pages'                                       => 'pages',
     'page_previous'                               => 'Page précédente',
-    'pass_changer' => "Changer votre mot de passe",
-    'poursuivre' => 'Poursuivre la lecture',
+    'pass_changer'                                => "Changer votre mot de passe",
+    'poursuivre'                                  => 'Poursuivre la lecture',
     'publication_about'                           => "À propos de l’article",
     'published_in'                                => 'Publiée dans',
     'published_date'                              => 'Publiée le',
-    'published_date_alt' => "Publié le",
-
-    'publie_en_ligne' => "Publié en ligne le",
+    'published_date_alt'                          => "Publié le",
+    
+    'publie_en_ligne'                             => "Publié en ligne le",
     
     // R
     'read_also'                                   => 'Lire également',
     'redaction_en_chef'                           => "Rédaction en chef",
-    'regarder' => "Regarder",
-    'recherche_aucun_resultat' => "Aucun résultat",
-    'recherche_resultat' => '1 résultat',
-    'recherche_resultats' => '@nb@ résultats',
-    'recherche_titre' => "Recherche",
+    'regarder'                                    => "Regarder",
+    'recherche_aucun_resultat'                    => "Aucun résultat",
+    'recherche_resultat'                          => '1 résultat',
+    'recherche_resultats'                         => '@nb@ résultats',
+    'recherche_titre'                             => "Recherche",
     
     // S
     'section_number'                              => '1 rubrique',
     'sections_number'                             => '@nb@ rubriques',
-    'semaine_precedente' => "Semaine précédente",
-    'semaine_suivante' => "Semaine suivante",
+    'semaine_precedente'                          => "Semaine précédente",
+    'semaine_suivante'                            => "Semaine suivante",
     
     // T
     'table_of_content'                            => "Sommaire",
     'tag_plural'                                  => 'Mots-clés',
     'tag_singular'                                => 'Mot-clé',
-    'titre_admin_vignettes' => "Vidéos : vignettes auto",
-    'tri_alpha' => 'Ordre alphabétique',
-    'tri_date' => 'Date de publication',
-    'tri_label' => 'Trier par',
-    'tri_label_alt' => 'Afficher par',
-    'tri_nom' => "Nom",
-    'tri_pertinence' => 'Pertinence',
-    'tri_titre' => 'Titre',
+    'theme'                                       => "Thème",
+    'themes'                                      => "Thèmes",
+    'titre_admin_vignettes'                       => "Vidéos : vignettes auto",
+    'tri_alpha'                                   => 'Ordre alphabétique',
+    'tri_date'                                    => 'Date de publication',
+    'tri_label'                                   => 'Trier par',
+    'tri_label_alt'                               => 'Afficher par',
+    'tri_nom'                                     => "Nom",
+    'tri_pertinence'                              => 'Pertinence',
+    'tri_titre'                                   => 'Titre',
     
     // V
     'version_online'                              => "Version en ligne",
