d3370bc6e2407c895d54e1a2515662b96f0b9db8
christophe le drean <christophe@rouge-rose.net>
2018-01-11 17:32:22 +0100

    Utiliser InuitCss version 6
    Pour le moment, il s'agit d'ajouter la version 6 et de garder la version
    5 également. Cela se traduit par un fichier css assez lourd.
    L'espoir est de :
    1- faire les ajouts à venir avec des styles plus
    cohérents sur la base de la v6 ;
    2- faire évoluer le reste du code au fur et à mesure (hum...)

--- gulpfile.js
+++ gulpfile.js
@@ -13,121 +13,116 @@
 //
 // *************************************
 
-
 "use strict";
-
 
 // -------------------------------------
 // Plugins
 // -------------------------------------
 
-const gulp            = require("gulp");
-const autoprefixer    = require("gulp-autoprefixer");
-const sass            = require("gulp-sass");
-const cleanCSS        = require("clean-css");
-const map             = require("vinyl-map");
-const rename          = require("gulp-rename");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const sass = require("gulp-sass");
+const cleanCSS = require("clean-css");
+const map = require("vinyl-map");
+const rename = require("gulp-rename");
 const plumberNotifier = require("gulp-plumber-notifier");
 // const concat          = require("gulp-concat");
-
 
 // -------------------------------------
 // Options
 // -------------------------------------
 
 var options = {
+	// ----- Default ----- //
+	default: {
+		tasks: ["compile:scss"]
+	},
 
-  // ----- Default ----- //
-  default: {
-    tasks: ['compile:scss']
-  },
+	// ----- SCSS ----- //
+	scss: {
+		paths: [
+			"./node_modules/",
+			"./bower_components/"
+			// 'bower_components/bourbon/dist',
+			// 'bower_components/inuit.css',
+			// 'bower_components/fontawesome/scss'
+		],
+		file: "_src/scss/app.scss",
+		files: "_src/scss/**/*.scss",
+		destination: "css"
+	},
 
-  // ----- SCSS ----- //
-  scss: {
-    paths       : [
-      './node_modules/',
-	  './bower_components/'
-      // 'bower_components/bourbon/dist',
-      // 'bower_components/inuit.css',
-      // 'bower_components/fontawesome/scss'
-    ],
-    file        : '_src/scss/app.scss',
-    files       : '_src/scss/**/*.scss',
-    destination : 'css'
-  },
+	// ----- CSS ----- //
+	css: {
+		file: "css/app.css",
+		destination: "css"
+	},
 
-  // ----- CSS ----- //
-  css: {
-    file        : 'css/app.css',
-    destination : 'css'
-  },
-
-  // ----- Watch ----- //
-  watch: {
-    files: function () {
-      return [
-        options.scss.files
-      ]
-    },
-    run: function () {
-      return [
-        ['compile:scss', 'minify:css']
-      ]
-    }
-  }
+	// ----- Watch ----- //
+	watch: {
+		files: function() {
+			return [options.scss.files];
+		},
+		run: function() {
+			return [["compile:scss", "minify:css"]];
+		}
+	}
 };
-
 
 // -------------------------------------
 // Task : default
 // -------------------------------------
 
-gulp.task( 'default', options.default.tasks );
-
+gulp.task("default", options.default.tasks);
 
 // -------------------------------------
 // Task: compile:scss
 // -------------------------------------
 
-gulp.task( 'compile:scss', function () {
-  gulp.src( options.scss.file )
-    .pipe( plumberNotifier() )
-    .pipe( sass({
-      includePaths: options.scss.paths,
-      outputStyle: 'compact'
-    }) )
-    .pipe( autoprefixer({
-      browsers: [ 'last 2 versions' ]
-    }) )
-    .pipe( gulp.dest(options.scss.destination) )
+gulp.task("compile:scss", function() {
+	gulp
+		.src(options.scss.file)
+		.pipe(plumberNotifier())
+		.pipe(
+			sass({
+				includePaths: options.scss.paths,
+				sourceComments: true,
+				outputStyle: "compact"
+			})
+		)
+		.pipe(
+			autoprefixer({
+				browsers: ["last 2 versions"]
+			})
+		)
+		.pipe(gulp.dest(options.scss.destination));
 });
-
 
 // -------------------------------------
 // Task : minify:css
 // -------------------------------------
 
-gulp.task( 'minify:css', function () {
-  var minify = map( function (buff, filename) {
-    return new cleanCSS({
-      keepBreaks: false
-    }).minify( buff.toString() ).styles
-  });
+gulp.task("minify:css", function() {
+	var minify = map(function(buff, filename) {
+		return new cleanCSS({
+			keepBreaks: false
+		}).minify(buff.toString()).styles;
+	});
 
-  return gulp.src( options.css.file )
-    .pipe( minify )
-    .pipe( rename({ suffix: '.min' }) )
-    .pipe( gulp.dest(options.css.destination) )
+	return gulp
+		.src(options.css.file)
+		.pipe(minify)
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(gulp.dest(options.css.destination));
 });
-
 
 // -------------------------------------
 // Task : watch
 // -------------------------------------
 
-gulp.task('watch', function() {
-  var watchFiles = options.watch.files();
-  watchFiles.forEach( function( files, index ) {
-    gulp.watch( files, options.watch.run()[ index ]  );
-  });
+gulp.task("watch", function() {
+	var watchFiles = options.watch.files();
+	watchFiles.forEach(function(files, index) {
+		gulp.watch(files, options.watch.run()[index]);
+	});
 });
