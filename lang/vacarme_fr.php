8e9002a6e6f917adfb3103a72e127331672f9b14
christophe le drean <christophe@rouge-rose.net>
2018-03-31 19:16:50 +0200

    Design récapitulatif abonnement

--- lang/vacarme_fr.php
+++ lang/vacarme_fr.php
@@ -4,19 +4,24 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
-	'abonnement_etape_cadeau_titre' => "Votre cadeau.",
-	'abonnement_etape_cadeau_desc' => "Pour vous remercier de votre abonnement, Vacarme vous offre un cadeau.",
-	'abonnement_erreur_etape_numero' => "Il y a un problème : des informations sont manquantes ou incohérentes. <br /> Veuillez d'abord choisir <a href='@url@'>un abonnement parmi ceux disponibles</a>.",
-	'abonnement_etape_abonnement_titre' => "Choisissez votre abonnement",
-	'abonnement_etape_abonnement_desc' => "Veuillez choisir votre abonnement selon votre localisation : France (Métropole et Outre-mer) ou International.",
-	'abonnement_etape_numero_titre' => "Choisissez votre premier numéro",
-	'abonnement_region_France' => "France <span class='u-hide-until-desktop'>(métropole et outre-mer)</span>",
-	'abonnement_region_International' => "International",
-	'abonnement_sans_cadeau_desc' => "Si aucun de ces cadeaux ne vous intéressent. Vous pouvez poursuivre la sélection de votre abonnement sans cadeau.",
-	'abonnement_sans_cadeau_titre' => "Pas de cadeau",
+	'abonnement_etape_cadeau_titre'      => "Votre cadeau.",
+	'abonnement_etape_cadeau_desc'       => "Pour vous remercier de votre abonnement, Vacarme vous offre un cadeau.",
+	'abonnement_erreur_etape_numero'     => "Il y a un problème : des informations sont manquantes ou incohérentes. <br /> Veuillez d'abord choisir <a href='@url@'>un abonnement parmi ceux disponibles</a>.",
+	'abonnement_etape_abonnement_titre'  => "Choisissez votre abonnement",
+	'abonnement_etape_abonnement_desc'   => "Veuillez choisir votre abonnement selon votre localisation : France (Métropole et Outre-mer) ou International.",
+	'abonnement_etape_numero_titre'      => "Choisissez votre premier numéro",
+	'abonnement_progression_label_abonnement' => "Abonnement : ",
+	'abonnement_progression_label_cadeau' => "Cadeau : ",
+	'abonnement_progression_label_numero' => "À partir du numéro : ",
+	'abonnement_progression_cadeau_vide' => "Aucun",
+	'abonnements_progression_titre'      => "Votre choix",
+	'abonnement_region_France'           => "France <span class='u-hide-until-desktop'>(métropole et outre-mer)</span>",
+	'abonnement_region_International'    => "International",
+	'abonnement_sans_cadeau_desc'        => "Si aucun de ces cadeaux ne vous intéressent. Vous pouvez poursuivre la sélection de votre abonnement sans cadeau.",
+	'abonnement_sans_cadeau_titre'       => "Pas de cadeau",
 	
 	// C
-	'choisir' => "Choisir",
+	'choisir'             => "Choisir",
 	'choisir_avec_cadeau' => "Choisir ce cadeau",
 	'choisir_sans_cadeau' => "Poursuivre sans cadeau",
 );
