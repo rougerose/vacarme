f767d82c4cf393aadde342059d6cedcddb31de7b
christophe le drean <christophe@rouge-rose.net>
2018-04-14 14:56:58 +0200

    Fonctions de traitement du nom et prénom
    Les fonctions sont maintenant déclarées par le plugin vinscriptions

--- vacarme_fonctions.php
+++ vacarme_fonctions.php
@@ -1,48 +1,6 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
-function filtre_prenom_nom($texte) {
-  return prenom_nom($texte);
-}
-
-function prenom_nom($texte) {
-    if (strstr($texte, "*")) {
-        $prenom = prenom($texte);
-        $nom = nom($texte);
-        if ($prenom && $nom) {
-            return $prenom.'&nbsp;'.$nom;
-        } else {
-            $prenom.$nom;
-        }
-    } else {
-        return $texte;
-    }
-}
-
-function prenom($texte) {
-    if(strstr($texte,"*")) {
-        if ($prenom = trim(preg_replace('#^(.*)\*(.*)#', '$2', $texte))) {
-            return $prenom;
-        } else {
-            return "";
-        }
-    } else {
-        return "";
-    }
-}
-
-function nom($texte) {
-    if(strstr($texte,"*")) {
-        if ($nom = trim(preg_replace('#^(.*)\*(.*)#', '$1', $texte))) {
-            return $nom;
-        } else {
-            return "";
-        }
-
-    } else {
-        return "";
-    }
-}
 
 // Supprimer les caractères accentués sur la première lettre d'une chaîne.
 // Si couper = non, la chaîne est reconstituée sur 31 caractères.
