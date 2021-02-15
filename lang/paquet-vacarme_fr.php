5a9070fe9ca26be4b935c05bfa3df39f36297b89
christophe le drean <christophe@rouge-rose.net>
2021-02-15 15:35:31 +0100

    Fichier langue

---
+++ lang/paquet-vacarme_fr.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// V
+	'vacarme_description' => '',
+	'vacarme_nom' => 'Vacarme',
+	'vacarme_slogan' => '',
+);
