c853a8bdc1168595eb1a9eee16b4f34319b95982
christophe le drean <christophe@rouge-rose.net>
2023-12-10 14:11:41 +0100

    chore: masquer options de debug

--- vacarme_options.php
+++ vacarme_options.php
@@ -7,13 +7,13 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS['z_blocs'] = ['content', 'bando', 'head', 'head_js', 'header', 'panier', 'aside', 'footer'];
 
 // Debug
-error_reporting(E_ALL^E_NOTICE);
-ini_set ("display_errors", "On");
-define('SPIP_ERREUR_REPORT', E_ALL);
-define('_NO_CACHE', -1);
-define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
-define('_LOG_FILELINE',true);
-define('_LOG_FILTRE_GRAVITE',8);
+// error_reporting(E_ALL^E_NOTICE);
+// ini_set ("display_errors", "On");
+// define('SPIP_ERREUR_REPORT', E_ALL);
+// define('_NO_CACHE', -1);
+// define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
+// define('_LOG_FILELINE',true);
+// define('_LOG_FILTRE_GRAVITE',8);
 
 // intertitres
 $GLOBALS['debut_intertitre'] = "\n<h2>";
